c77ed55b3993867af57728a589057289f4ac564c
rogwel <anxielworld@gmail.com>
2025-04-01 17:32:58 +0300

    [groceries]: {Add commit to the piscine rust repository. Add library file containing the functions insert and at_index}

--- groceries/src/lib.rs
+++ groceries/src/lib.rs
@@ -2,6 +2,6 @@ pub fn insert(vec: &mut Vec<String>, val: String) {
     vec.push(val);
 }
 
-pub fn at_index(slice: &[String], index: usize) -> Option<&str> {
-    slice.get(index).map(|s| s.as_str())
+pub fn at_index(slice: &[String], index: usize) -> &str {
+    slice.get(index).map(|s| s.as_str()).expect("Index out of bounds.")
 }
