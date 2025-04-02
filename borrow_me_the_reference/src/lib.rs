ce5c3b8d9654033b2c41dab350f7f35f31a0f233
anxielray <anxielworld@gmail.com>
2025-04-02 11:33:41 -0400

    [memory]: {Add commit to the piscine rust repository. Add the solution of the challenge in the borrow me the referrence exercise in the memory quest}

--- borrow_me_the_reference/src/lib.rs
+++ borrow_me_the_reference/src/lib.rs
@@ -1,25 +1,38 @@
 pub fn delete_and_backspace(s: &mut String) {
-    let mut result = Vec::new();
-    let mut skip_next = false;
+    let mut result = String::new();
+    let mut chars = s.chars().peekable();
+    let mut skip_count = 0;
 
-    for c in s.chars() {
-        if skip_next {
-            skip_next = false;
+    while let Some(c) = chars.next() {
+        if skip_count > 0 {
+            skip_count -= 1;
             continue;
         }
+
         match c {
             '-' => {
-                if !result.is_empty() {
-                    result.pop();
-                }
+                result.pop(); // Remove the last character for backspace
             }
             '+' => {
-                skip_next = true;
+                // Count consecutive '+' signs
+                skip_count += 1;
+                while let Some(next_char) = chars.peek() {
+                    if *next_char == '+' {
+                        skip_count += 1;
+                        chars.next();
+                    } else {
+                        break;
+                    }
+                }
+                // Skip characters equal to the number of consecutive '+' signs
             }
-            _ => result.push(c),
+            _ => {
+                result.push(c); // Add current character to result
+            }
         }
     }
-    *s = result.into_iter().collect();
+
+    *s = result;
 }
 
 pub fn do_operations(v: &mut [String]) {
@@ -27,7 +40,7 @@ pub fn do_operations(v: &mut [String]) {
         if let Some(operator_pos) = eq.find(|c| c == '+' || c == '-') {
             let (left, right) = eq.split_at(operator_pos);
             let operator = eq.chars().nth(operator_pos).unwrap();
-            let right = &right[1..];
+            let right = &right[1..]; // Skip the operator itself
             let left_num: i32 = left.parse().unwrap();
             let right_num: i32 = right.parse().unwrap();
             let result = match operator {
@@ -35,7 +48,7 @@ pub fn do_operations(v: &mut [String]) {
                 '-' => left_num - right_num,
                 _ => unreachable!(),
             };
-            *eq = result.to_string();
+            *eq = result.to_string(); // Replace equation with result
         }
     }
 }
