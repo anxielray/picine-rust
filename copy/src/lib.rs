5bb388df9de0b5ebc690dc8ac0e85197dd168928
anxielray <anxielworld@gmail.com>
2025-04-02 10:34:55 -0400

    [memory]: {Add commit to the piscine rust repository. Add file containing the solution to the copy challengge in the memory quest}

--- copy/src/lib.rs
+++ copy/src/lib.rs
@@ -1,20 +1,28 @@
-use std::f64;
-
 pub fn nbr_function(c: i32) -> (i32, f64, f64) {
-    let exp_val = f64::exp(c as f64);
-    let ln_val = if c == 0 { f64::NEG_INFINITY } else { (c.abs() as f64).ln() };
-    (c, exp_val, ln_val)
+    let exp = (c as f64).exp();
+    let ln = if c == 0 {
+        f64::NEG_INFINITY
+    } else {
+        (c.abs() as f64).ln()
+    };
+    (c, exp, ln)
 }
 
-pub fn str_function(a: &str) -> (String, String) {
-    let exp_values = a.split_whitespace().map(|num_str| {
-        let num: f64 = num_str.parse().unwrap();
-        f64::exp(num)
-    }).map(|exp_val| exp_val.to_string()).collect::<Vec<String>>().join(" ");
-    (a.to_string(), exp_values)
+pub fn str_function(a: String) -> (String, String) {
+    let exp_values = a.split_whitespace()
+        .map(|s| s.parse::<f64>().unwrap().exp().to_string())
+        .collect::<Vec<_>>()
+        .join(" ");
+    (a, exp_values)
 }
 
-pub fn vec_function(b: &Vec<i32>) -> (Vec<i32>, Vec<f64>) {
-    let ln_values = b.iter().map(|&num| if num == 0 { f64::NEG_INFINITY } else { (num.abs() as f64).ln() }).collect::<Vec<f64>>();
-    (b.to_vec(), ln_values)
+pub fn vec_function(b: Vec<i32>) -> (Vec<i32>, Vec<f64>) {
+    let ln_values = b.iter()
+        .map(|&n| if n == 0 {
+            f64::NEG_INFINITY
+        } else {
+            (n.abs() as f64).ln()
+        })
+        .collect();
+    (b, ln_values)
 }
