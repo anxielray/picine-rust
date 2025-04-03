41cf2467f5633c87b1809a39a71cf12ae5c9b68e
rogwel <anxielworld@gmail.com>
2025-04-03 19:16:47 +0300

    [Momeory]: {Add commit to the piscine rust repository. Add the solution corrected update for the arrange it exercise}

--- arrange_it/src/lib.rs
+++ arrange_it/src/lib.rs
@@ -1,19 +1,17 @@
 pub fn arrange_phrase(phrase: &str) -> String {
     let mut words: Vec<&str> = phrase.split_whitespace().collect();
-    
+
     words.sort_by_key(|word| {
         word.chars()
-        .find(|c| c.is_digit(10))
-        .unwrap_or('0')
-        .to_digit(10)
-        .unwrap_or(0)
+            .find(|c| c.is_digit(10))
+            .unwrap_or('0')
+            .to_digit(10)
+            .unwrap_or(0)
     });
 
-    remove_numbers(words.join(" "))
-}
-
-pub fn remove_numbers(input: String) -> String {
-    input.chars()
+    words
+        .join(" ")
+        .chars()
         .filter(|c| !c.is_digit(10))
         .collect()
 }
