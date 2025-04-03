1fd21cc48a975594350932741f6a5e6be5c10183
anxielray <anxielworld@gmail.com>
2025-04-03 15:47:28 -0400

    [Momeory]: {Add commit to the piscine rust repository. Update the exercises in the DATA quest removing the binaries and the cargo.lock files from the repository to avoid congestion and clutter of code}

--- DATA/arrange_it/target/debug/deps/arrange_it-43e366f160961811.d
+++
@@ -1,5 +0,0 @@
-/home/anxiel/projects/gitea/piscine-rust/arrange_it/target/debug/deps/libarrange_it-43e366f160961811.rmeta: src/lib.rs
-
-/home/anxiel/projects/gitea/piscine-rust/arrange_it/target/debug/deps/arrange_it-43e366f160961811.d: src/lib.rs
-
-src/lib.rs:
