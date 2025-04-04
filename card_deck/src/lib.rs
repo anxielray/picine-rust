133a6dbc3a04947f10e06cffcba8b480d9a7d6f1
anxielray <anxielworld@gmail.com>
2025-04-04 10:39:14 -0400

    [Momeory]: {Add commit to the piscine rust repository. Add the solution to the card deck exercise in data  quest}

--- card_deck/src/lib.rs
+++ card_deck/src/lib.rs
@@ -1,6 +1,6 @@
-use rand::Rng;
+use rand::random_range;
 
-// Suit enum: Represents the four suits in a deck of cards.
+#[derive(Debug, PartialEq)]
 pub enum Suit {
     Heart,
     Diamond,
@@ -9,31 +9,23 @@ pub enum Suit {
 }
 
 impl Suit {
-    // Randomly selects and returns a suit.
     pub fn random() -> Suit {
-        let mut rng = rand::thread_rng();
-        match rng.gen_range(1..=4) {
-            1 => Suit::Heart,
-            2 => Suit::Diamond,
-            3 => Suit::Spade,
-            4 => Suit::Club,
-            _ => panic!("Unexpected value"),
-        }
+        let value = random_range(1..=4);
+        Suit::translate(value)
     }
 
-    // Translates a number (1-4) to a corresponding suit.
     pub fn translate(value: u8) -> Suit {
         match value {
             1 => Suit::Heart,
             2 => Suit::Diamond,
             3 => Suit::Spade,
             4 => Suit::Club,
-            _ => panic!("Invalid suit value"),
+            _ => panic!("Invalid suit value: {}", value),
         }
     }
 }
 
-// Rank enum: Represents the ranks in a deck, including Ace, Jack, Queen, King, and numbers 2-10.
+#[derive(Debug, PartialEq)]
 pub enum Rank {
     Ace,
     Number(u8),
@@ -43,41 +35,106 @@ pub enum Rank {
 }
 
 impl Rank {
-    // Randomly selects and returns a rank.
     pub fn random() -> Rank {
-        let mut rng = rand::thread_rng();
-        match rng.gen_range(1..=13) {
-            1 => Rank::Ace,
-            11 => Rank::Jack,
-            12 => Rank::Queen,
-            13 => Rank::King,
-            n => Rank::Number(n),
-        }
+        let value = random_range(1..=13);
+        Rank::translate(value)
     }
 
-    // Translates a number (1-13) to a corresponding rank.
     pub fn translate(value: u8) -> Rank {
         match value {
             1 => Rank::Ace,
             11 => Rank::Jack,
             12 => Rank::Queen,
             13 => Rank::King,
-            n if n >= 2 && n <= 10 => Rank::Number(n),
-            _ => panic!("Invalid rank value"),
+            2..=10 => Rank::Number(value),
+            _ => panic!("Invalid rank value: {}", value),
         }
     }
 }
 
-// Card struct: Represents a single card in the deck, consisting of a rank and a suit.
+#[derive(Debug)]
 pub struct Card {
     pub suit: Suit,
     pub rank: Rank,
 }
 
-// Function that checks if the card is the Ace of Spades.
-pub fn winner_card(card: &Card) -> bool {
-    match (card.rank, card.suit) {
-        (Rank::Ace, Suit::Spade) => true,
-        _ => false,
+pub fn winner_card(card: Card) -> bool {
+    card.suit == Suit::Spade && card.rank == Rank::Ace
+}
+
+
+/*
+A standard deck of cards has 52 cards: 4 suits with 13 cards per suit. Represent the cards from a deck:
+
+    Create an enum to represent the Suit.
+    Implement the associated function random, which returns a random Suit (Heart, Diamond, Spade or Club).
+    Create a Rank enum. For ace and face cards, it can be one of Ace, King, Queen or Jack. For the values from 2 to 10, it can have a Number value associated to a u8.
+    Create an associated function to Rank called Random that returns a random Rank.
+    Create a structure name Card which has the fields suit and rank.
+
+Define:
+
+    The associated function translate for Rank and Suit:
+        For Suit, translate converts an integer value (u8) to a suit (1 -> Heart, 2 -> Diamonds, 3 -> Spade, 4 -> Club).
+        For Rank, translate converts an integer value (u8) to a rank ( 1 -> Ace, 2 -> 2, .., 10 -> 10, 11 -> Jack, 12 -> Queen, 13 -> King).
+    The associated function random for Rank and Suit returns a random Rank and Suit respectively.
+    Finally define the function winner_card which returns true if the card passed as an argument is an ace of spades.
+
+Expected Functions and Structures
+
+pub enum Suit {
+}
+
+pub enum Rank {
+}
+
+impl Suit {
+    pub fn random() -> Suit {
+    }
+
+    pub fn translate(value: u8) -> Suit {
     }
 }
+
+impl Rank {
+    pub fn random() -> Rank {
+    }
+
+    pub fn translate(value: u8) -> Rank {
+    }
+}
+
+pub struct Card {
+    pub suit: Suit,
+    pub rank: Rank,
+}
+
+pub fn winner_card(card: &Card) -> bool {
+}
+
+Usage
+
+Here is a program to test your function
+
+use card_deck::*;
+
+fn main() {
+    let your_card = Card {
+        rank: Rank::random(),
+        suit: Suit::random(),
+    };
+
+    println!("Your card is {:?}", your_card);
+
+    if card_deck::winner_card(your_card) {
+        println!("You are the winner!");
+    }
+}
+
+And its output
+
+$ cargo run
+Your card is Card { suit: Club, rank: Ace }
+$
+
+*/
