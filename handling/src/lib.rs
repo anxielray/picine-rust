a4224e519df46d8b4b1006f8f9845f4385671403
anxielray <anxielworld@gmail.com>
2025-04-08 18:58:08 -0400

    [Momeory]: {Add commit to the piscine rust repository. handling}

--- handling/src/lib.rs
+++ handling/src/lib.rs
@@ -4,17 +4,10 @@ use std::path::Path;
 
 pub fn open_or_create<P: AsRef<Path>>(path: &P, content: &str) {
     let mut file = OpenOptions::new()
-        .write(true)
         .append(true)
         .create(true)
         .open(path)
-        .unwrap_or_else(|err| {
-            eprintln!("Error opening or creating file: {}", err);
-            panic!("File operation failed");
-        });
+        .expect("Failed to open or create file");
 
-    if let Err(err) = writeln!(file, "{}", content) {
-        eprintln!("Error writing to file: {}", err);
-        panic!("File write failed");
-    }
+    writeln!(file, "{}", content).expect("Failed to write to the file");
 }
