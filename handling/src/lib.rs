ee2166cbfe7cc9629efe80d5fb1b3e5fc280d673
rogwel <anxielworld@gmail.com>
2025-04-08 15:12:42 +0300

    [Data]: {Add commit to the piscine rust repository. handling}

---
+++ handling/src/lib.rs
@@ -0,0 +1,20 @@
+use std::fs::{self, OpenOptions};
+use std::io::{self, Write};
+use std::path::Path;
+
+pub fn open_or_create<P: AsRef<Path>>(path: &P, content: &str) {
+    let mut file = OpenOptions::new()
+        .write(true)
+        .append(true)
+        .create(true)
+        .open(path)
+        .unwrap_or_else(|err| {
+            eprintln!("Error opening or creating file: {}", err);
+            panic!("File operation failed");
+        });
+
+    if let Err(err) = writeln!(file, "{}", content) {
+        eprintln!("Error writing to file: {}", err);
+        panic!("File write failed");
+    }
+}
