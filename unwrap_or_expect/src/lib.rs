e8e7487c888074474ea1a19fc777a678f8752d4a
anxielray <anxielworld@gmail.com>
2025-04-08 18:38:26 -0400

    [Momeory]: {Add commit to the piscine rust repository. unwrap_or_expected}

--- unwrap_or_expect/src/lib.rs
+++ unwrap_or_expect/src/lib.rs
@@ -6,31 +6,18 @@ pub enum Security {
     UnexpectedUrl,
 }
 
-// This is already internally defined by the rust compiler, no need to include it here
-// enum Result<T, E> {
-//     Ok(T),
-//     Err(E),
-// }
-
 pub fn fetch_data(server: Result<&str, &str>, security_level: Security) -> String {
-    match server {
-        Ok(url) => {
-            match security_level {
-                Security::Unknown => url.to_string(),
-                Security::Message => url.to_string(),
-                Security::Warning => url.to_string(),
-                Security::NotFound => url.to_string(),
-                Security::UnexpectedUrl => url.to_string(),
-            }
-        }
-        Err(error_message) => {
-            match security_level {
-                Security::Unknown => panic!("{}", error_message),
-                Security::Message => panic!("ERROR: program stops"),
-                Security::Warning => format!("WARNING: check the server"),
-                Security::NotFound => format!("Not found: {}", error_message),
-                Security::UnexpectedUrl => panic!("{}", error_message),
-            }
-        }
+    match security_level {
+        Security::Unknown => server.unwrap().to_string(),
+        Security::Message => server.expect("ERROR: program stops").to_string(),
+        Security::Warning => server.unwrap_or("WARNING: check the server").to_string(),
+        Security::NotFound => match server {
+            Ok(url) => url.to_string(),
+            Err(e) => format!("Not found: {}", e),
+        },
+        Security::UnexpectedUrl => match server {
+            Ok(_) => panic!("Expected error but got Ok"),
+            Err(e) => panic!("{}", e),
+        },
     }
 }
