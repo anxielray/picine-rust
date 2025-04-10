bd0dbbaf819ce37795706f605d137dc6a37300dd
rogwel <anxielworld@gmail.com>
2025-04-10 15:12:39 +0300

    [Crates]: {Add commit to the piscine rust repository. middle_day}

--- middle_day/src/lib.rs
+++ middle_day/src/lib.rs
@@ -1,17 +1,18 @@
-use chrono::{NaiveDate, Datelike, Weekday as wd};
+use chrono::{Datelike, NaiveDate, Weekday as wd};
 
 pub fn middle_day(year: i32) -> Option<wd> {
-    if year < 1 || year > 9999 {
-        return None;
-    }
-    let leap_year = (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
-    let days_in_year = if leap_year { 366 } else { 365 };
+    let days_in_year = if NaiveDate::from_ymd_opt(year, 12, 31).unwrap().leap_year() {
+        366
+    } else {
+        365
+    };
 
     if days_in_year % 2 == 0 {
         return None;
     }
 
-    let middle_day = NaiveDate::from_ymd(year, 7, 2);
+    let middle_day =
+        NaiveDate::from_ymd_opt(year, 1, 1).unwrap() + chrono::Duration::days(days_in_year / 2);
     Some(middle_day.weekday())
 }
 
