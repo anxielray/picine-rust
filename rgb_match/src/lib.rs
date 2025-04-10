5e1ec581c21b439b0f411c9439c6f221801fbbfe
anxielray <anxielworld@gmail.com>
2025-04-10 19:48:07 -0400

    [Data]: {Add commit to the piscine rust repository. rgb}

---
+++ rgb_match/src/lib.rs
@@ -0,0 +1,31 @@
+pub struct Color {
+    pub r: u8,
+    pub g: u8,
+    pub b: u8,
+    pub a: u8,
+}
+
+impl Color {
+    pub fn swap(mut self, first: u8, second: u8) -> Color {
+        let r = self.r;
+        let g = self.g;
+        let b = self.b;
+        let a = self.a;
+
+        match first {
+            r => self.r = second,
+            g => self.g = second,
+            b => self.b = second,
+            a => self.a = second,
+            _ => panic!("Invalid color value: {}", first),
+        }
+        match second {
+            r => self.r = first,
+            g => self.g = first,
+            b => self.b = first,
+            a => self.a = first,
+            _ => panic!("Invalid color value: {}", second),
+        }
+        self
+    }
+}
