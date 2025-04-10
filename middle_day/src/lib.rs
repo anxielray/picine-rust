4b0f684f0ee53398e3941ded6e3df368f1d853f7
rogwel <anxielworld@gmail.com>
2025-04-10 14:51:28 +0300

    [Crates]: {Add commit to the piscine rust repository. middle_day}

--- middle_day/src/lib.rs
+++ middle_day/src/lib.rs
@@ -14,3 +14,7 @@ pub fn middle_day(year: i32) -> Option<wd> {
     let middle_day = NaiveDate::from_ymd_opt(year, 1, 1).unwrap() + chrono::Duration::days(days_in_year / 2);
     Some(middle_day.weekday())
 }
+
+// later implementation;
+// use the method .ordinal()
+// this method will directly return the number of days given a date object
