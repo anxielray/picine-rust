016180c9bf5dbda7136cc546dad0b5aa8e88b3f1
anxielray <anxielworld@gmail.com>
2025-04-10 21:12:22 -0400

    [Data]: {Add commit to the piscine rust repository. scytale cipher }

--- scytale_cipher/src/lib.rs
+++ scytale_cipher/src/lib.rs
@@ -1,17 +1,5 @@
-pub fn scytale_cipher(message: String, size: u32) -> String {
-    let size = size as usize;
-    let chars: Vec<char> = message.chars().collect();
-    let length = chars.len();
+#[path = "encode.rs"]
+mod encode;
 
-    let mut result = String::new();
-
-    for col in 0..size {
-        let mut idx = col;
-        while idx < length {
-            result.push(chars[idx]);
-            idx += size;
-        }
-    }
-
-    result
-}
+#[test]
+fn encoding() {}
