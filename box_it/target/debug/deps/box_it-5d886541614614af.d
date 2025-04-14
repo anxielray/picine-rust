e7110b9576440582bd79b7969a45c405541a725a
anxielray <anxielworld@gmail.com>
2025-04-14 19:35:32 -0400

    Add the solution to the exercise of box it in the pointers quest

--- box_it/target/debug/deps/box_it-5d886541614614af.d
+++
@@ -1,5 +0,0 @@
-/home/anxiel/projects/gitea/piscine-rust/box_it/target/debug/deps/libbox_it-5d886541614614af.rmeta: src/lib.rs
-
-/home/anxiel/projects/gitea/piscine-rust/box_it/target/debug/deps/box_it-5d886541614614af.d: src/lib.rs
-
-src/lib.rs:
--- box_it/target/debug/deps/box_it-60972871e69fafbf.d
+++
@@ -1,5 +0,0 @@
-/home/anxiel/projects/gitea/piscine-rust/box_it/target/debug/deps/libbox_it-60972871e69fafbf.rmeta: src/main.rs
-
-/home/anxiel/projects/gitea/piscine-rust/box_it/target/debug/deps/box_it-60972871e69fafbf.d: src/main.rs
-
-src/main.rs:
